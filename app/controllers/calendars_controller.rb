14f5566b1f05851f622e1eedbd9f9d1736eba96f
hiro-22 <70711048+hiro-22@users.noreply.github.com>
2020-10-05 14:06:01 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,10 +20,11 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    #wdays[Date.today.wday] => wdays[1] => (月)
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例) 今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例) 今日が2月1日の場合・・・ Date.today.day => 1日   Date.today.wday => 1
 
     @week_days = []
 
@@ -34,7 +35,9 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      #Date.today.wdayを利用して添字となる数値を得る    Date.today.wday => 1
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:wdays[(@todays_date+x).wday] }
       @week_days.push(days)
     end
 
